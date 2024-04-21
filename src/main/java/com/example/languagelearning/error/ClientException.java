595b87ec6fa07f1314675b1afc3c4862718a42c4
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-04-21 14:30:01 +0200

    added: exception handling

---
+++ src/main/java/com/example/languagelearning/error/ClientException.java
@@ -0,0 +1,8 @@
+package com.example.languagelearning.error;
+
+public class ClientException extends RuntimeException{
+
+    public ClientException(String message) {
+        super(message);
+    }
+}
