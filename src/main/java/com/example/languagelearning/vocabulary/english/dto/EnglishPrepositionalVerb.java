4f89ac5ed98b79bd9f3af44843067d676913ce86
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-05-01 17:22:38 +0200

    removed 'preposition' field

--- src/main/java/com/example/languagelearning/vocabulary/english/dto/EnglishPrepositionalVerb.java
+++ src/main/java/com/example/languagelearning/vocabulary/english/dto/EnglishPrepositionalVerb.java
@@ -1,7 +1,7 @@
 package com.example.languagelearning.vocabulary.english.dto;
 
 public record EnglishPrepositionalVerb(
-        String englishPrepositionalVerb, String translation, String preposition,
+        String englishPrepositionalVerb, String translation,
         String englishDefinition, String englishExampleSentence, boolean isFormal
 ) {
 }
