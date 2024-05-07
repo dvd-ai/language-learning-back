630a5895518e57652b383d02cd5fc02c60d1bb1f
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-05-07 10:52:08 +0200

    added open api specifications dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	id("org.flywaydb.flyway") version "9.0.1"
 	id("org.springframework.boot") version "3.2.4"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("org.springdoc.openapi-gradle-plugin") version "1.8.0"
 }
 
 extra["springAiVersion"] = "0.8.1"
@@ -24,6 +25,7 @@ dependencies {
 	implementation ("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.ai:spring-ai-openai-spring-boot-starter")
 	implementation("org.flywaydb:flyway-core:9.0.1")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 	implementation("io.hypersistence:hypersistence-utils-hibernate-60:3.7.2")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
