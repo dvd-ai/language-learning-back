016a1aa6f73e2d6c7aa62137c47bf093c507a5e9
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-05-07 09:34:54 +0200

    update: field name (isFormal -> isColloquial) in dto,changed prompt for the new field.

--- src/main/java/com/example/languagelearning/vocabulary/english/dto/EnglishPrepositionalVerb.java
+++ src/main/java/com/example/languagelearning/vocabulary/english/dto/EnglishPrepositionalVerb.java
@@ -2,6 +2,6 @@ package com.example.languagelearning.vocabulary.english.dto;
 
 public record EnglishPrepositionalVerb(
         String englishPrepositionalVerb, String translation,
-        String englishDefinition, String englishExampleSentence, boolean isFormal
+        String englishDefinition, String englishExampleSentence, boolean isColloquial
 ) {
 }
--- src/main/java/com/example/languagelearning/vocabulary/english/dto/EnglishVerb.java
+++ src/main/java/com/example/languagelearning/vocabulary/english/dto/EnglishVerb.java
@@ -8,6 +8,6 @@ public record EnglishVerb(
         String englishDefinition,
         String preposition,
         String englishExampleSentence,
-        boolean isFormal
+        boolean isColloquial
 ) {
 }
