b87101473e43f2303233291689ca8be6252856e6
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-05-11 10:58:23 +0200

    added field translationLanguage

--- src/main/resources/db/migration/V20240421120548__create_table_english_vocabulary_topic_entity.sql
+++ src/main/resources/db/migration/V20240421120548__create_table_english_vocabulary_topic_entity.sql
@@ -1,5 +1,6 @@
 CREATE TABLE english_vocabulary_topic_entity (
   id BIGINT NOT NULL,
    english_vocabulary_topic JSONB,
+   translation_language VARCHAR(255) NOT NULL,
    CONSTRAINT pk_englishvocabularytopicentity PRIMARY KEY (id)
 );
