0feea9be8f2c6a59fd7787c09183d39f647b4744
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-05-21 17:03:38 +0200

    added overridden missing request parameter handler

--- src/main/java/com/example/languagelearning/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/languagelearning/error/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.example.languagelearning.error;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
@@ -16,6 +20,19 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         ErrorDto errorDto = new ErrorDto(ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDto);
     }
+
+    @Override
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
+                                                                          HttpHeaders headers, HttpStatusCode status,
+                                                                          WebRequest request
+    ) {
+        String message = String.format("Missing request parameter: %s", ex.getParameterName());
+        ErrorDto errorDto = new ErrorDto(message);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDto);
+    }
+
+
 
     @ExceptionHandler(value = {ClientException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
