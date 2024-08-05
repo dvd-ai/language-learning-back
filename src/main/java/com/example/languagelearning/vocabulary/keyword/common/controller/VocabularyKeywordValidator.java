b80cf0e139915c896fb75e4bb418769d01bff297
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-08-05 12:11:05 +0200

    added checkAllTranslationLanguages

--- src/main/java/com/example/languagelearning/vocabulary/keyword/common/controller/VocabularyKeywordValidator.java
+++ src/main/java/com/example/languagelearning/vocabulary/keyword/common/controller/VocabularyKeywordValidator.java
@@ -1,8 +1,10 @@
 package com.example.languagelearning.vocabulary.keyword.common.controller;
 
 import com.example.languagelearning.error.ClientException;
+import com.example.languagelearning.vocabulary.keyword.common.dto.VocabularyTopicDto;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Locale;
 
 import static com.example.languagelearning.openai.gpt.GptSupportedLanguages.isLanguageSupported;
@@ -17,6 +19,12 @@ public class VocabularyKeywordValidator {
     private void checkTranslationLanguage(Locale translationLanguage) {
         if (!isLanguageSupported(translationLanguage)) {
             throw new ClientException(String.format("the translationLanguage %s is not supported by GPT", translationLanguage));
+        }
+    }
+
+    public void checkAllTranslationLanguages(List<? extends VocabularyTopicDto> dtos) {
+        for (VocabularyTopicDto dto : dtos) {
+            checkTranslationLanguage(dto.getTranslationLanguage());
         }
     }
 }
