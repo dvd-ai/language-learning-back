f21da424a7b12bade0dad859226849f7630bbb38
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-08-05 12:43:48 +0200

    update de, en mappers for their new dtos

--- src/main/java/com/example/languagelearning/vocabulary/keyword/english/EnglishVocabularyMapper.java
+++ src/main/java/com/example/languagelearning/vocabulary/keyword/english/EnglishVocabularyMapper.java
@@ -1,25 +1,54 @@
 package com.example.languagelearning.vocabulary.keyword.english;
 
 import com.example.languagelearning.vocabulary.keyword.english.dto.EnglishVocabularyTopic;
+import com.example.languagelearning.vocabulary.keyword.english.dto.EnglishVocabularyTopicDto;
 import com.example.languagelearning.vocabulary.keyword.english.entity.EnglishVocabularyTopicEntity;
+
+import java.util.Locale;
+
+import static com.example.languagelearning.common.LanguageUtil.normalizeLocale;
 
 public class EnglishVocabularyMapper {
 
     public EnglishVocabularyMapper() {
     }
 
-    public EnglishVocabularyTopic mapToDto(EnglishVocabularyTopicEntity entity) {
+    public EnglishVocabularyTopicDto mapToDto(EnglishVocabularyTopicEntity entity) {
         EnglishVocabularyTopic vocabularyTopic = entity.getEnglishVocabularyTopic();
 
-        return new EnglishVocabularyTopic(
-                vocabularyTopic.getVerbs(),
-                vocabularyTopic.getNouns(),
-                vocabularyTopic.getAdjectives(),
-                vocabularyTopic.getCollocations(),
-                vocabularyTopic.getIdioms(),
-                vocabularyTopic.getPrepositionalVerbs(),
-                vocabularyTopic.getPhrasalVerbs(),
-                vocabularyTopic.getVocabularyName()
+        return new EnglishVocabularyTopicDto(
+                new Locale("en"),
+                new Locale(entity.getTranslationLanguage()),
+                entity.getId(),
+                new EnglishVocabularyTopic(
+                        vocabularyTopic.getVerbs(),
+                        vocabularyTopic.getNouns(),
+                        vocabularyTopic.getAdjectives(),
+                        vocabularyTopic.getCollocations(),
+                        vocabularyTopic.getIdioms(),
+                        vocabularyTopic.getPrepositionalVerbs(),
+                        vocabularyTopic.getPhrasalVerbs(),
+                        vocabularyTopic.getVocabularyName()
+                )
+        );
+    }
+
+    public EnglishVocabularyTopicEntity mapToEntity(EnglishVocabularyTopicDto dto) {
+        EnglishVocabularyTopic vocabularyTopic = dto.getEnglishVocabularyTopic();
+
+        return new EnglishVocabularyTopicEntity(
+                dto.getId(),
+                new EnglishVocabularyTopic(
+                        vocabularyTopic.getVerbs(),
+                        vocabularyTopic.getNouns(),
+                        vocabularyTopic.getAdjectives(),
+                        vocabularyTopic.getCollocations(),
+                        vocabularyTopic.getIdioms(),
+                        vocabularyTopic.getPrepositionalVerbs(),
+                        vocabularyTopic.getPhrasalVerbs(),
+                        vocabularyTopic.getVocabularyName()
+                ),
+                normalizeLocale(dto.getTranslationLanguage())
         );
     }
 }
--- src/main/java/com/example/languagelearning/vocabulary/keyword/german/GermanVocabularyMapper.java
+++ src/main/java/com/example/languagelearning/vocabulary/keyword/german/GermanVocabularyMapper.java
@@ -1,25 +1,52 @@
 package com.example.languagelearning.vocabulary.keyword.german;
 
 import com.example.languagelearning.vocabulary.keyword.german.dto.GermanVocabularyTopic;
+import com.example.languagelearning.vocabulary.keyword.german.dto.GermanVocabularyTopicDto;
 import com.example.languagelearning.vocabulary.keyword.german.entity.GermanVocabularyTopicEntity;
+
+import java.util.Locale;
+
+import static com.example.languagelearning.common.LanguageUtil.normalizeLocale;
 
 public class GermanVocabularyMapper {
 
     public GermanVocabularyMapper() {
     }
 
-    public GermanVocabularyTopic mapToDto(GermanVocabularyTopicEntity entity) {
+    public GermanVocabularyTopicDto mapToDto(GermanVocabularyTopicEntity entity) {
         GermanVocabularyTopic vocabularyTopic = entity.getGermanVocabularyTopic();
 
-        return new GermanVocabularyTopic(
-                vocabularyTopic.getVocabularyName(),
-                vocabularyTopic.getVerbs(),
-                vocabularyTopic.getNouns(),
-                vocabularyTopic.getAdjectives(),
-                vocabularyTopic.getCollocations(),
-                vocabularyTopic.getIdioms(),
-                vocabularyTopic.getPrepositionalVerbs()
+        return new GermanVocabularyTopicDto(
+                new Locale("de"),
+                new Locale(entity.getTranslationLanguage()),
+                entity.getId(),
+                new GermanVocabularyTopic(
+                        vocabularyTopic.getVocabularyName(),
+                        vocabularyTopic.getVerbs(),
+                        vocabularyTopic.getNouns(),
+                        vocabularyTopic.getAdjectives(),
+                        vocabularyTopic.getCollocations(),
+                        vocabularyTopic.getIdioms(),
+                        vocabularyTopic.getPrepositionalVerbs()
+                )
+        );
+    }
 
+    public GermanVocabularyTopicEntity mapToEntity(GermanVocabularyTopicDto dto) {
+        GermanVocabularyTopic vocabularyTopic = dto.getGermanVocabularyTopic();
+
+        return new GermanVocabularyTopicEntity(
+                dto.getId(),
+                new GermanVocabularyTopic(
+                        vocabularyTopic.getVocabularyName(),
+                        vocabularyTopic.getVerbs(),
+                        vocabularyTopic.getNouns(),
+                        vocabularyTopic.getAdjectives(),
+                        vocabularyTopic.getCollocations(),
+                        vocabularyTopic.getIdioms(),
+                        vocabularyTopic.getPrepositionalVerbs()
+                ),
+                normalizeLocale(dto.getTranslationLanguage())
         );
     }
 }
