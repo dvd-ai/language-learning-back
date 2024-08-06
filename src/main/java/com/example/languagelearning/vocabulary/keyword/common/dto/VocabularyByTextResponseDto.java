60643bae818cd69fe71dbb2a530ddadeb1dd7801
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-08-06 15:39:15 +0200

    added VocabularyByTextResponseDto

---
+++ src/main/java/com/example/languagelearning/vocabulary/keyword/common/dto/VocabularyByTextResponseDto.java
@@ -0,0 +1,9 @@
+package com.example.languagelearning.vocabulary.keyword.common.dto;
+
+import java.util.List;
+import java.util.Map;
+
+public class VocabularyByTextResponseDto {
+    private Map<Integer, List<String>>wordsForParagraphs;
+    VocabularyTopicDto vocabularyTopicDto;
+}
