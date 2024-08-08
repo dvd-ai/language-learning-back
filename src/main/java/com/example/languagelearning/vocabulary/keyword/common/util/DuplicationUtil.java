ecfa1745f52b4812866d4dcb369c25030c9df47a
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-08-08 17:31:28 +0200

    added DuplicationUtil

---
+++ src/main/java/com/example/languagelearning/vocabulary/keyword/common/util/DuplicationUtil.java
@@ -0,0 +1,27 @@
+package com.example.languagelearning.vocabulary.keyword.common.util;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class DuplicationUtil {
+
+    private DuplicationUtil() {
+    }
+
+    public static <T> boolean isDuplicated (List<T> list, T item) {
+        return list
+                .stream()
+                .filter(i -> i.equals(item))
+                .count() > 1;
+    }
+    public static <T> void removeDuplicates(List<T> items) {
+        Iterator<T> iterator = items.iterator();
+        while (iterator.hasNext()) {
+            T item = iterator.next();
+            if (isDuplicated(items, item)) {
+                iterator.remove();
+            }
+        }
+    }
+
+}
