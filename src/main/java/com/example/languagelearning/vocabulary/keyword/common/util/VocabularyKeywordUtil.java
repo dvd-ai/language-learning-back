f609178cb96551d97d19dea61f7fbae90af14d64
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-08-08 09:40:05 +0200

    added common getSpeechPartJson method

--- src/main/java/com/example/languagelearning/vocabulary/keyword/common/util/VocabularyKeywordUtil.java
+++ src/main/java/com/example/languagelearning/vocabulary/keyword/common/util/VocabularyKeywordUtil.java
@@ -1,6 +1,7 @@
 package com.example.languagelearning.vocabulary.keyword.common.util;
 
 import com.example.languagelearning.openai.OpenAiService;
+import com.example.languagelearning.vocabulary.keyword.common.prompt.PromptParameters;
 import com.example.languagelearning.vocabulary.keyword.common.prompt.VocabularyKeywordPromptParameters;
 import org.springframework.scheduling.annotation.Async;
 
@@ -15,6 +16,12 @@ public class VocabularyKeywordUtil {
     @Async
     public static CompletableFuture<String> getSpeechPartJson(OpenAiService openAiService, VocabularyKeywordPromptParameters promptParameters,
                                                               Function<VocabularyKeywordPromptParameters, String> speechPartDefinition) {
+        return openAiService.defaultAsyncCall(speechPartDefinition.apply(promptParameters));
+    }
+
+    @Async
+    public static CompletableFuture<String> getSpeechPartJson(OpenAiService openAiService, PromptParameters promptParameters,
+                                                              Function<PromptParameters, String> speechPartDefinition) {
         return openAiService.defaultAsyncCall(speechPartDefinition.apply(promptParameters));
     }
 }
