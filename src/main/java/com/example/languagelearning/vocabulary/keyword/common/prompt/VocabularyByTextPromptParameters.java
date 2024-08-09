f6774b102dc264fb07a0daaabea135b65503b88e
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-08-09 16:01:38 +0200

    added VocabularyByTextPromptParameters

--- src/main/java/com/example/languagelearning/vocabulary/keyword/common/prompt/VocabularyByTextPromptParameters.java
+++ src/main/java/com/example/languagelearning/vocabulary/keyword/common/prompt/VocabularyByTextPromptParameters.java
@@ -3,6 +3,6 @@ package com.example.languagelearning.vocabulary.keyword.common.prompt;
 import com.example.languagelearning.vocabulary.keyword.common.dto.VocabularyByTextRequestDto;
 
 public record VocabularyByTextPromptParameters(
-        VocabularyByTextRequestDto requestDto, String text
-) implements PromptParameters {
+        VocabularyByTextRequestDto requestDto, String text, Integer textNumber
+) {
 }
