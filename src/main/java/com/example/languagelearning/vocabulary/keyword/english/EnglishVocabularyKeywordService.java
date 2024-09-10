948d871fdf2185ecbf41975b50565da0083969f9
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-09-10 12:55:54 +0200

    EnglishVocabularyKeywordService uses external classes to delegate the implementations

--- src/main/java/com/example/languagelearning/vocabulary/keyword/english/EnglishVocabularyKeywordService.java
+++ src/main/java/com/example/languagelearning/vocabulary/keyword/english/EnglishVocabularyKeywordService.java
@@ -1,54 +1,46 @@
 package com.example.languagelearning.vocabulary.keyword.english;
 
-import com.example.languagelearning.error.ApplicationException;
+import com.example.languagelearning.common.EnglishLanguage;
 import com.example.languagelearning.openai.OpenAiService;
+import com.example.languagelearning.vocabulary.common.english.EnglishVocabularyMapper;
 import com.example.languagelearning.vocabulary.keyword.common.VocabularyKeywordService;
 import com.example.languagelearning.vocabulary.keyword.common.dto.Subtopic1NestingLevelBlockContainer;
-import com.example.languagelearning.vocabulary.by_text.common.dto.VocabularyByTextRequestDto;
-import com.example.languagelearning.vocabulary.keyword.common.dto.VocabularyTopicComparator;
-import com.example.languagelearning.vocabulary.keyword.common.dto.VocabularyTopicDto;
-import com.example.languagelearning.vocabulary.keyword.common.prompt.VocabularyByTextPromptParameters;
+
+import com.example.languagelearning.vocabulary.common.VocabularyTopicDto;
 import com.example.languagelearning.vocabulary.keyword.common.prompt.VocabularyKeywordPromptParameters;
 import com.example.languagelearning.vocabulary.keyword.common.prompt.VocabularySubtopic1LevelPromptProcessor;
 import com.example.languagelearning.vocabulary.keyword.english.dto.EnglishVocabularyTopic;
 import com.example.languagelearning.vocabulary.keyword.english.dto.EnglishVocabularyTopicDto;
-import com.example.languagelearning.vocabulary.keyword.english.dto.container.*;
 import com.example.languagelearning.vocabulary.keyword.english.entity.EnglishVocabularyTopicEntity;
-import com.example.languagelearning.vocabulary.keyword.english.prompt.EnglishVocabularyByTextPromptProcessor;
-import com.example.languagelearning.vocabulary.keyword.english.prompt.EnglishVocabularyPromptProcessor;
 import com.example.languagelearning.vocabulary.keyword.english.repo.EnglishVocabularyTopicEntityService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.ai.openai.OpenAiChatOptions;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
-import static com.example.languagelearning.common.LanguageUtil.normalizeLocale;
+
 import static com.example.languagelearning.util.CompletableFutureUtil.extractValuesFromCompletableFutures;
-import static com.example.languagelearning.util.CompletableFutureUtil.tryToExtractSingleCompletedFutureElement;
-import static com.example.languagelearning.util.TextUtil.breakTextIntoSentencesParts;
-import static com.example.languagelearning.util.TextUtil.breakTextIntoSortedParagraphs;
-import static com.example.languagelearning.vocabulary.keyword.common.util.VocabularyKeywordUtil.getSpeechPartJson;
-import static com.example.languagelearning.vocabulary.keyword.english.EnglishVocabularyTopicPostProcessor.performCleanup;
+import static com.example.languagelearning.vocabulary.common.english.EnglishVocabularyTopicPostProcessor.performCleanup;
+
 
 @Service
 public class EnglishVocabularyKeywordService extends EnglishLanguage implements VocabularyKeywordService {
 
     private final ObjectMapper objectMapper;
     private final EnglishVocabularyTopicEntityService vocabularyTopicEntityService;
+    private final EnglishVocabularyKeywordCfTopicUtil cfUtil;
 
     private final EnglishVocabularyMapper vocabularyMapper;
     private final VocabularySubtopic1LevelPromptProcessor subtopic1LevelPromptProcessor;
 
-    public EnglishVocabularyKeywordService(ObjectMapper objectMapper, EnglishVocabularyTopicEntityService vocabularyTopicEntityService, EnglishVocabularyMapper vocabularyMapper, VocabularySubtopic1LevelPromptProcessor subtopic1LevelPromptProcessor) {
+    public EnglishVocabularyKeywordService(ObjectMapper objectMapper, EnglishVocabularyTopicEntityService vocabularyTopicEntityService, EnglishVocabularyKeywordCfTopicUtil cfUtil, EnglishVocabularyMapper vocabularyMapper, VocabularySubtopic1LevelPromptProcessor subtopic1LevelPromptProcessor) {
         this.objectMapper = objectMapper;
         this.vocabularyTopicEntityService = vocabularyTopicEntityService;
+        this.cfUtil = cfUtil;
         this.vocabularyMapper = vocabularyMapper;
         this.subtopic1LevelPromptProcessor = subtopic1LevelPromptProcessor;
     }
